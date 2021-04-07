ec764b6f43afb9dd53b5b9753e7d76a0e95fd576
Yusuf Umar <yusufumaris@gmail.com>
2021-04-07 20:45:10 +0700

    Add ambient occlusion option when doing quick setup

--- Root.py
+++ Root.py
@@ -447,6 +447,7 @@ class YQuickYPaintNodeSetup(bpy.types.Operator):
             #update=update_quick_setup_type)
 
     color = BoolProperty(name='Color', default=True)
+    ao = BoolProperty(name='Ambient Occlusion', default=False)
     metallic = BoolProperty(name='Metallic', default=True)
     roughness = BoolProperty(name='Roughness', default=True)
     normal = BoolProperty(name='Normal', default=True)
@@ -495,6 +496,7 @@ class YQuickYPaintNodeSetup(bpy.types.Operator):
             ccol = col.column(align=True)
             ccol.label(text='Channels:')
 
+            ccol.label(text='')
             if self.type == 'PRINCIPLED':
                 ccol.label(text='')
             ccol.label(text='')
@@ -505,6 +507,7 @@ class YQuickYPaintNodeSetup(bpy.types.Operator):
         if self.type != 'EMISSION':
             ccol = col.column(align=True)
             ccol.prop(self, 'color', toggle=True)
+            ccol.prop(self, 'ao', toggle=True)
             if self.type == 'PRINCIPLED':
                 ccol.prop(self, 'metallic', toggle=True)
             ccol.prop(self, 'roughness', toggle=True)
@@ -548,6 +551,7 @@ class YQuickYPaintNodeSetup(bpy.types.Operator):
         trans_bsdf = None
         mix_bsdf = None
         mat_out = None
+        ao_mul = None
 
         # Get active output
         output = [n for n in nodes if n.type == 'OUTPUT_MATERIAL' and n.is_active_output]
@@ -590,6 +594,14 @@ class YQuickYPaintNodeSetup(bpy.types.Operator):
                 output.is_active_output = False
                 mat_out.location = output.location.copy()
                 mat_out.location.x += 180
+
+        if self.ao and not ao_mul and self.type != 'EMISSION':
+            ao_mul = nodes.new('ShaderNodeMixRGB')
+            ao_mul.inputs[0].default_value = 1.0
+            ao_mul.blend_type = 'MULTIPLY'
+
+            ao_mul.location = mat_out.location.copy()
+            mat_out.location.x += 180
 
         if transp_node_needed and not mix_bsdf:
             mix_bsdf = nodes.new('ShaderNodeMixShader')
@@ -640,6 +652,7 @@ class YQuickYPaintNodeSetup(bpy.types.Operator):
 
         # Add new channels
         ch_color = None
+        ch_ao = None
         ch_metallic = None
         ch_roughness = None
         ch_normal = None
@@ -648,6 +661,9 @@ class YQuickYPaintNodeSetup(bpy.types.Operator):
             ch_color = create_new_yp_channel(group_tree, 'Color', 'RGB', non_color=False)
 
         if self.type != 'EMISSION':
+            if self.ao:
+                ch_ao = create_new_yp_channel(group_tree, 'Ambient Occlusion', 'RGB', non_color=True)
+
             if self.type == 'PRINCIPLED' and self.metallic:
                 ch_metallic = create_new_yp_channel(group_tree, 'Metallic', 'VALUE', non_color=True)
 
@@ -660,18 +676,32 @@ class YQuickYPaintNodeSetup(bpy.types.Operator):
         # Update io
         check_all_channel_ios(group_tree.yp)
 
+        # HACK: Remap channel pointers, because sometimes pointers are lost at this time
+        ch_color = group_tree.yp.channels.get('Color')
+        ch_ao = group_tree.yp.channels.get('Ambient Occlusion')
+        ch_metallic = group_tree.yp.channels.get('Metallic')
+        ch_roughness = group_tree.yp.channels.get('Roughness')
+        ch_normal = group_tree.yp.channels.get('Normal')
+
         if ch_color:
             inp = main_bsdf.inputs[0]
             set_input_default_value(node, ch_color, inp.default_value)
-            #links.new(node.outputs[ch_color.io_index], inp)
-            links.new(node.outputs[ch_color.name], inp)
+            if ch_ao and ao_mul:
+                links.new(node.outputs[ch_color.name], ao_mul.inputs[1])
+                links.new(node.outputs[ch_ao.name], ao_mul.inputs[2])
+                links.new(ao_mul.outputs[0], inp)
+            else:
+                links.new(node.outputs[ch_color.name], inp)
+
             # Enable, link, and disable alpha to remember which input was alpha connected to
             ch_color.enable_alpha = True
-            #links.new(node.outputs[ch_color.io_index+1], mix_bsdf.inputs[0])
             if transp_node_needed:
                 links.new(node.outputs[ch_color.name+io_suffix['ALPHA']], mix_bsdf.inputs[0])
             else: links.new(node.outputs[ch_color.name+io_suffix['ALPHA']], main_bsdf.inputs['Alpha'])
             ch_color.enable_alpha = False
+
+        if ch_ao:
+            set_input_default_value(node, ch_ao, (1,1,1))
 
         if ch_metallic:
             inp = main_bsdf.inputs['Metallic']
@@ -694,6 +724,9 @@ class YQuickYPaintNodeSetup(bpy.types.Operator):
         # Set new yp node location
         if output:
             node.location = main_bsdf.location.copy()
+            if ao_mul: 
+                ao_mul.location.y = node.location.y
+                node.location.x -= 180
             main_bsdf.location.x += 180
             if transp_node_needed:
                 trans_bsdf.location.x += 180
@@ -707,6 +740,9 @@ class YQuickYPaintNodeSetup(bpy.types.Operator):
             mat_out.location.y += 300
             node.location = main_bsdf.location.copy()
             node.location.x -= 180
+            if ao_mul: 
+                ao_mul.location.y = node.location.y
+                node.location.x -= 180
 
         # Disable overlay on Blender 2.8
         if is_greater_than_280() and self.mute_texture_paint_overlay:
