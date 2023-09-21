403606968cd56008733de6ad4f22a3662f893103
Yusuf Umar <yusufumaris@gmail.com>
2023-09-21 12:49:26 +0700

    Add new contributor credit

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 bl_info = {
     "name": "Ucupaint",
-    "author": "Yusuf Umar, Agni Rakai Sahakarya, Jan Bláha",
+    "author": "Yusuf Umar, Agni Rakai Sahakarya, Jan Bláha, Ahmad Rifai",
     "version": (1, 1, 0),
     "blender": (2, 80, 0),
     "location": "Node Editor > Properties > Ucupaint",
