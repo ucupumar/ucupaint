a44da24ba5c73defad68fa95b5cc3a4098632045
森雨Plus <morirain@outlook.com>
2024-01-06 03:25:06 +0800

    Experimental: baking channel to vertex color can support Alpha channel (#79)
    * Experimental: baking channel to vertex color can support Alpha channel
    
    * Using numpy to bake alpha of vertex colors

--- bake_common.py
+++ bake_common.py
@@ -1124,7 +1124,7 @@ def fxaa_image(image, alpha_aware=True, bake_device='GPU', first_tile_only=False
 
     return image
 
-def bake_to_vcol(mat, node, root_ch, extra_channel=None, extra_multiplier=1.0):
+def bake_to_vcol(mat, node, root_ch, objs, extra_channel=None, extra_multiplier=1.0, bake_alpha=False, bake_to_vcol=''):
 
     # Create setup nodes
     emit = mat.node_tree.nodes.new('ShaderNodeEmission')
@@ -1168,8 +1168,51 @@ def bake_to_vcol(mat, node, root_ch, extra_channel=None, extra_multiplier=1.0):
 
     mat.node_tree.links.new(rgb, emit.inputs[0])
 
+    # To avoid duplicate code, define the function here
+    def bake_alpha_to_vcol():
+        temp_vcol_alpha_name = '__temp__ucupaint_vertex_color_for_alpha_bake'
+        for obj in objs:
+            # Creates temp vertex color for baking alpha
+            temp_vcol = new_vertex_color(obj, temp_vcol_alpha_name)
+            set_active_vertex_color(obj, temp_vcol)
+        bpy.ops.object.bake()
+        for obj in objs:
+            vcols = get_vertex_colors(obj)
+            temp_vcol = vcols.get(temp_vcol_alpha_name)
+            target_vcol = vcols.get(bake_to_vcol)
+            
+            # Speed up the process with numpy
+            dim_rgba = 4
+            temp_nvcol = numpy.zeros(len(temp_vcol.data) * dim_rgba, dtype=numpy.float32)
+            target_nvcol = numpy.zeros(len(target_vcol.data) * dim_rgba, dtype=numpy.float32)
+            
+            temp_vcol.data.foreach_get('color', temp_nvcol)
+            target_vcol.data.foreach_get('color', target_nvcol)
+            temp_nvcol2D = temp_nvcol.reshape(-1, dim_rgba)
+            target_nvcol2D = target_nvcol.reshape(-1, dim_rgba)
+
+            # Moves the alpha of the temp vertex color to the target vertex color
+            target_nvcol2D[:, 3] = temp_nvcol2D[:, 0]
+            target_vcol.data.foreach_set('color', target_nvcol)   
+
+            # Deletes the temp vertex color and resets the active vertex color
+            vcols.remove(temp_vcol)
+            set_active_vertex_color(obj, target_vcol)
+
     # Bake!
-    bpy.ops.object.bake()
+    # When bake_alpha is True and the channel type is 'VALUE', bake the alpha channel separately.
+    if bake_alpha and root_ch.type == 'VALUE':
+        bake_alpha_to_vcol()
+    else:
+        # Bake without alpha channel
+        bpy.ops.object.bake()
+    
+    # If bake_alpha is True and the channel type is 'RGB', Bake twice to merge Alpha channel
+    if bake_alpha and root_ch.type == 'RGB' and root_ch.enable_alpha:
+        # Connect channel alpha channel
+        alpha_outp = node.outputs.get(root_ch.name + io_suffix['ALPHA'])
+        mat.node_tree.links.new(alpha_outp, emit.inputs[0])
+        bake_alpha_to_vcol()
 
     # Remove temp nodes
     simple_remove_node(mat.node_tree, emit)
