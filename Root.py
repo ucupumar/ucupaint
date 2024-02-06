8935fd014a3f1560f7974a0739c9fc165f632631
Yusuf Umar <yusufumaris@gmail.com>
2024-02-06 20:49:54 +0700

    Update image editor image when changing active material

--- Root.py
+++ Root.py
@@ -3370,6 +3370,7 @@ class YPaintWMProps(bpy.types.PropertyGroup):
     clipboard_layer : StringProperty(default='')
 
     last_object : StringProperty(default='')
+    last_material : StringProperty(default='')
     last_mode : StringProperty(default='')
 
     edit_image_editor_area_index : IntProperty(default=-1)
@@ -3437,10 +3438,12 @@ def ypaint_last_object_update(scene):
     except: return
     if not obj: return
 
+    mat = obj.active_material
     ypwm = bpy.context.window_manager.ypprops
 
-    if ypwm.last_object != obj.name:
+    if ypwm.last_object != obj.name or (mat and mat.name != ypwm.last_material):
         ypwm.last_object = obj.name
+        ypwm.last_material = mat.name
         node = get_active_ypaint_node()
 
         # Refresh layer index to update editor image
