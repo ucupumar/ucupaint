bd5560ef3feca4f1710192400ea434ff679a239b
Yusuf Umar <yusufumaris@gmail.com>
2026-05-27 21:26:09 +0700

    Fix: Using custom image for bump channel will make the UI disappear if smooth(er) bump is enabled

--- common.py
+++ common.py
@@ -2409,7 +2409,8 @@ def get_channel_source_tree(ch, layer=None, tree=None):
 
     if ch.source_group != '':
         source_group = tree.nodes.get(ch.source_group)
-        return source_group.node_tree
+        if source_group:
+            return source_group.node_tree
 
     return tree
 
