87bbb87987b889db15dc4c435c29a998b92b3b35
lsmine0 <40209533+lsmine0@users.noreply.github.com>
2026-09-17 16:53:16 +0200

    Ucupaint Expanded Decal Projections (#421)
    * BasicDecal reimplemented
    
    * Added cylinder and sphere decal
    
    * node adjust and gizmo added
    
    * Remove Experimental name
    
    * Re-Added missing comments
    
    * Clear Residual Rename files
    
    * Clear residual changes from test
    
    * Rewrite and reimplementation in lib.blend to fit with current style and standard.
    
    * Clear out node_arrangements.py
    
    * Code cleanup
    
    * Adjusted Decal nodes layout to be readable
    
    * Decal.py Cleanup
    
    * check_entity_decal_nodes refactoring
    
    * Decal Cleanup final
    
    * Delte lib.blend1 and residual files
    
    * Update ui for mask decals
    
    * Node arrangment and UI fix
    
    * Remove Match to be compatible with older python versions
    
    * Code Cleanup
    
    * Gizmo version adjustment
    
    * Fix: Decal do not persist on toggle layer
    
    * Decal Scale Cleanup
    
    * full ui and Decal redo
    
    * Final cleanup
    
    * Residual cleanup
    
    * Fix: Property rename
    
    * Fix: Decal scale appear on Flat
    
    * Fix: revert Decal Process name to keep compatiblity
    
    * Residual cleanup
    
    * Fix: node connection layer to mask missmatch
    
    * Fix: 'ScaleX' to 'Scale X' change in lib blend
    
    * Fix: decal_scale min max default and precision set
    
    * Fix: input output missmatch from layer to mask
    
    * Fix: Decal scale on flat doesn't adjust for image ratio
    
    * Decal_process arragment adjustment
    
    * Merging Scaling attempt
    
    * lib decal default fix
    
    * Decal Node Scale Merge Final
    
    * Code Cleanup
    
    * Layer input dynamic socket editing.
    
    * Fix some issues:
    - Decal scale input on disabled entity still exist when one of the axis value is above 1.0
    - Error when using decal texcoord on mask
    - Decal UI has some unnecessary elements
    - Decal constraints should not be usable on non-flat projection decal
    
    * lib.blend conflict resolution
    
    * Fix: Decal Process Cylinder Z scale
    
    * Fix: Value not saving between layer toggle
    
    * Fix: Scale does not save in previous versions
    
    ---------
    
    Co-authored-by: Yusuf Umar <yusufumaris@gmail.com>

--- node_arrangements.py
+++ node_arrangements.py
@@ -944,7 +944,10 @@ def rearrange_layer_nodes(layer, tree=None):
     #    loc.y -= 240
 
     if check_set_node_loc(tree, layer.decal_process, loc):
-        loc.y -= 170
+        if layer.decal_projection_type != 'FLAT':
+            loc.y -= 170
+        else:
+            loc.y -= 240
 
     if check_set_node_loc(tree, layer.texcoord, loc):
         loc.y -= 240
@@ -1190,7 +1193,7 @@ def rearrange_layer_nodes(layer, tree=None):
             loc.y -= 140
 
         if check_set_node_loc(tree, mask.decal_process, loc):
-            loc.y -= 170
+            loc.y -= 250
 
         if check_set_node_loc(tree, mask.uv_map, loc):
             loc.y -= 130
